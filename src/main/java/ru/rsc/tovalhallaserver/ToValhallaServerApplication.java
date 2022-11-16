671dab2737f788397b917bb9e8d1738630eb0457
Alexander Vetrenko <vetrenkosasha@gmail.com>
2022-11-17 03:12:37 +0500

    Initial commit

---
+++ src/main/java/ru/rsc/tovalhallaserver/ToValhallaServerApplication.java
@@ -0,0 +1,13 @@
+package ru.rsc.tovalhallaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToValhallaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToValhallaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/rsc/tovalhallaserver/ToValhallaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rsc.tovalhallaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToValhallaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
